789693d713f56ac945d7390c75ae325a40d5bb05
roee030 <roee.angel93@gmail.com>
2018-12-22 23:01:23 +0200

    Registertion setting

--- app/src/main/java/com/example/roeea/eventplanner/Activities/RegisterActivity.java
+++ app/src/main/java/com/example/roeea/eventplanner/Activities/RegisterActivity.java
@@ -1,12 +1,18 @@
 package com.example.roeea.eventplanner.Activities;
 
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.example.roeea.eventplanner.ObjectClasses.User;
 import com.example.roeea.eventplanner.R;
+import com.example.roeea.eventplanner.Server.MiniServer;
+import com.firebase.client.Firebase;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
@@ -16,21 +22,50 @@ import com.google.firebase.database.FirebaseDatabase;
 
 public class RegisterActivity extends AppCompatActivity {
 
-    FirebaseAuth Mauth;
-    DatabaseReference db;
+    private FirebaseAuth Mauth;
+    private DatabaseReference db;
+    private Firebase mRRef;
+    private FirebaseAuth mAuth;
+    private EditText Email;
+    private EditText Password;
+    private EditText Fullname;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
-
+        Password = (EditText)findViewById(R.id.passwordtextregister);
+        Fullname = (EditText)findViewById(R.id.fullnametextregister);
+        Email = (EditText)findViewById(R.id.emailtextregister);
         Mauth = FirebaseAuth.getInstance(); //Defines an instance
         db = FirebaseDatabase.getInstance().getReference(); //Getting root reference
         addUserToDB();
 
     }
 
+    public void onClick(View v)
+    {
+        switch (v.getId())
+        {
+            case(R.id.registerbutton):
+                User user = new User(Fullname.toString(),Password.toString(),Email.toString());
+                MiniServer server = MiniServer.getServerInstance();
+                boolean register_flag = server.checkRegister(user);
+                if(register_flag)
+                {
+                    db.child("User's").child(user.getEmail().replace(".", "|")).setValue(user);
+                }
+                else
+                {
+                    Toast.makeText(RegisterActivity.this,"Couldn't register",Toast.LENGTH_SHORT).show();
+                }
+            case(R.id.registerBackBT):
+                Intent register = new Intent(RegisterActivity.this,MainActivity.class);
+                default:
+                    break;
+        }
+    }
     public void addUserToDB()
     {
         Mauth.createUserWithEmailAndPassword("dorlevu121@www.com","123456").addOnCompleteListener(new OnCompleteListener<AuthResult>() {
--- app/src/main/java/com/example/roeea/eventplanner/Server/MiniServer.java
+++ app/src/main/java/com/example/roeea/eventplanner/Server/MiniServer.java
@@ -46,4 +46,5 @@ public class MiniServer {
         });
         return false;
     }
+
 }
